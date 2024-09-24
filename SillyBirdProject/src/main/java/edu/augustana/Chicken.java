38595107cbf75ba8c6abcb97c9d71268c23c43a7
vicflip <vmgurrola03@gmail.com>
2024-09-24 12:03:06 -0500

    Now with dancing chicken

---
+++ SillyBirdProject/src/main/java/edu/augustana/Chicken.java
@@ -0,0 +1,9 @@
+package edu.augustana;
+
+public class Chicken {
+
+    public static void dance(){
+        System.out.println("Chicken dance!");
+    }
+
+}
--- SillyBirdProject/src/main/java/edu/augustana/Main.java
+++ SillyBirdProject/src/main/java/edu/augustana/Main.java
@@ -2,6 +2,9 @@ package edu.augustana;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Chicken kfc = new Chicken();
+        kfc.dance();
+
     }
+
 }
